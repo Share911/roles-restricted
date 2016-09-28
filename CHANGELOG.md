4fee04a73ab27374c475138c3acbdef4197b6527
Loren Sands-Ramshaw <lorensr@gmail.com>
2016-09-28 19:10:49 -0400

    Release 0.1.5

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,12 @@
 All notable changes to this project will be documented in this file.
 This project adheres to [Semantic Versioning](http://semver.org/).
 
+## [0.1.5] - 2016-09-28
+### Fixed
+- Add check to avoid common error when first connecting (#2)
+### Updated
+- `login-links@0.1.3`
+
 ## [0.1.3] - 2016-05-02
 ### Added
 - `isUnrestrictedFromPublish`
--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'loren:roles-restricted',
-  version: '0.1.4',
+  version: '0.1.5',
   summary: 'Adds restricted-access state and autologin links to alanning:roles',
   git: 'https://github.com/lorensr/roles-restricted.git'
 });
@@ -16,7 +16,7 @@ Package.onUse(function(api) {
            'reactive-var',
            'random',
            'ddp',
-           'loren:login-links@0.1.2',
+           'loren:login-links@0.1.3',
            'alanning:roles@1.2.15'])
 
   api.export('Roles');
